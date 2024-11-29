5ef4794eb084791a288849de6100bd96140bd039
gp332868 <1194554295@qq.com>
2024-11-29 14:21:10 +0800

    Add files via upload
    sssss

---
+++ uw621d/uw621d/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uw621d
+//
+//  Created by guopeng on 2024/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ uw621d/uw621dUITests/uw621dUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  uw621dUITestsLaunchTests.swift
+//  uw621dUITests
+//
+//  Created by guopeng on 2024/11/29.
+//
+
+import XCTest
+
+final class uw621dUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
